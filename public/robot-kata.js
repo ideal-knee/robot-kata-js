ce06b295eab557aaf05f423b5454c0a8d5966673
Dan Kee <dan@dankee.com>
2013-08-30 21:23:48 +0000

    More refactor

--- public/robot-kata.js
+++ public/robot-kata.js
@@ -1,49 +1,47 @@
-loadFloorImage(function () {
-  console.log("Hello Robot Kata!");
-});
-
 var floor = {
   context: $('#floor')[0].getContext('2d'),
   offset: $('#floor').offset(),
-  getPosition: function (e) {
+  getEventPosition: function (e) {
     return {
       x: Math.floor(e.pageX - this.offset.left),
       y: Math.floor(e.pageY - this.offset.top)
     }
-  }
-}
+  },
+  getColor: function (pos) {
+    var imageData = this.context.getImageData(pos.x, pos.y, 1, 1).data;
+    var r = imageData[0];
+    var g = imageData[1];
+    var b = imageData[2];
+    var threshold = 192;
 
-function loadFloorImage(callback) {
-  var image = new Image();
-  image.src = 'roomba-dock.png';
-  image.onload = function () {
-    floor.context.drawImage(this, 0, 0);
-    callback();
-  }
-}
-
-function getColor(pos) {
-  var imageData = floor.context.getImageData(pos.x, pos.y, 1, 1).data;
-  var r = imageData[0];
-  var g = imageData[1];
-  var b = imageData[2];
-  var threshold = 192;
-
-  if (r > threshold && g > threshold && b > threshold) {
-    return 'white';
-  } else if (r > threshold && g > threshold) {
-    return 'yellow';
-  } else if (r > threshold) {
-    return 'red';
-  } else if (g > threshold) {
-    return 'green';
-  } else if (b > threshold) {
-    return 'blue';
-  } else {
-    return 'black';
+    if (r > threshold && g > threshold && b > threshold) {
+      return 'white';
+    } else if (r > threshold && g > threshold) {
+      return 'yellow';
+    } else if (r > threshold) {
+      return 'red';
+    } else if (g > threshold) {
+      return 'green';
+    } else if (b > threshold) {
+      return 'blue';
+    } else {
+      return 'black';
+    }
+  },
+  loadImage: function (callback) {
+    var image = new Image();
+    image.src = 'roomba-dock.png';
+    image.onload = function () {
+      floor.context.drawImage(this, 0, 0);
+      callback();
+    }
   }
 }
 
 $('#floor').mousemove(function (e) {
-  console.log(getColor(floor.getPosition(e)));
+  console.log(floor.getColor(floor.getEventPosition(e)));
+});
+
+floor.loadImage(function () {
+  console.log("Hello Robot Kata!");
 });
