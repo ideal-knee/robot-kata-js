e96654f01e83e85ab3c7a305c763f94305d47bef
Dan Kee <dan@dankee.com>
2013-09-01 09:16:28 -0500

    Functional refactor

--- public/robot-kata.js
+++ public/robot-kata.js
@@ -1,47 +1,67 @@
-var floor = {
-  context: $('#floor')[0].getContext('2d'),
-  offset: $('#floor').offset(),
-  getEventPosition: function (e) {
-    return {
-      x: Math.floor(e.pageX - this.offset.left),
-      y: Math.floor(e.pageY - this.offset.top)
-    }
-  },
-  getColor: function (pos) {
-    var imageData = this.context.getImageData(pos.x, pos.y, 1, 1).data;
-    var r = imageData[0];
-    var g = imageData[1];
-    var b = imageData[2];
-    var threshold = 192;
+var floorCanvas = document.getElementById('floor');
 
-    if (r > threshold && g > threshold && b > threshold) {
-      return 'white';
-    } else if (r > threshold && g > threshold) {
-      return 'yellow';
-    } else if (r > threshold) {
-      return 'red';
-    } else if (g > threshold) {
-      return 'green';
-    } else if (b > threshold) {
-      return 'blue';
-    } else {
-      return 'black';
-    }
-  },
-  loadImage: function (callback) {
-    var image = new Image();
-    image.src = 'roomba-dock.png';
-    image.onload = function () {
-      floor.context.drawImage(this, 0, 0);
-      callback();
-    }
+function get2DContext(canvas) {
+  return canvas.getContext('2d');
+}
+
+function getElementPosition(element) {
+  var offset = $(element).offset();
+  return { x: offset.left, y: offset.top };
+}
+
+function getEventPosition(event) {
+  return { x: event.pageX, y: event.pageY };
+}
+
+function getRelativePosition(origin, absolute) {
+  return { x: absolute.x - origin.x, y: absolute.y - origin.y };
+}
+
+function getPixelColor(context, position) {
+  var imageData = context.getImageData(position.x, position.y, 1, 1).data;
+  return { r: imageData[0], g: imageData[1], b: imageData[2] };
+}
+
+function getColorName(color) {
+  var threshold = 192;
+
+  if (color.r > threshold && color.g > threshold && color.b > threshold) {
+    return 'white';
+  } else if (color.r > threshold && color.g > threshold) {
+    return 'yellow';
+  } else if (color.r > threshold) {
+    return 'red';
+  } else if (color.g > threshold) {
+    return 'green';
+  } else if (color.b > threshold) {
+    return 'blue';
+  } else {
+    return 'black';
   }
 }
 
-$('#floor').mousemove(function (e) {
-  console.log(floor.getColor(floor.getEventPosition(e)));
-});
+function loadImage_(context, imageName, callback) {
+  var image = new Image();
+  image.src = imageName;
+  image.onload = function () {
+    context.drawImage(image, 0, 0);
+    callback();
+  }
+}
 
-floor.loadImage(function () {
-  console.log("Hello Robot Kata!");
-});
+function addMouseMoveCallback_(element, callback) {
+  $(element).mousemove(callback);
+}
+
+function logColorAtEvent_(event) {
+  console.log(getColorName(getPixelColor(get2DContext(floorCanvas),
+                                         getRelativePosition(getElementPosition(floorCanvas),
+                                                             getEventPosition(event) ) )));
+}
+
+function printWelcomeMessage_() {
+  console.log('Hello robot kata!');
+}
+
+loadImage_(get2DContext(floorCanvas), 'roomba-dock.png', printWelcomeMessage_);
+addMouseMoveCallback_(floorCanvas, logColorAtEvent_);
